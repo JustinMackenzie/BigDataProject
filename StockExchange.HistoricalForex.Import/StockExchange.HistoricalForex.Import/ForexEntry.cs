8a90b8cd915a4475e25d63fd40e952ac67095ea0
Justin Mackenzie <jmackenzie1908@gmail.com>
2017-03-24 13:10:31 -0400

    Implemented historical forex processing application.

---
+++ StockExchange.HistoricalForex.Import/StockExchange.HistoricalForex.Import/ForexEntry.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace StockExchange.HistoricalForex.Import
+{
+    public class ForexEntry
+    {
+        public float Low { get; set; }
+        public float High { get; set; }
+        public DateTime Timestamp { get; set; }
+
+        public ForexEntry(DateTime timestamp, float low, float high)
+        {
+            this.Timestamp = timestamp;
+            this.Low = low;
+            this.High = high;
+        }
+    }
+}
---
+++ StockExchange.HistoricalForex.Import/StockExchange.HistoricalForex.Import/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using Newtonsoft.Json;
+
+namespace StockExchange.HistoricalForex.Import
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string folderPath = args[0];
+
+            IEnumerable<string> files = Directory.GetFiles(folderPath, "*.csv").OrderBy(s => s);
+
+            Console.WriteLine("Starting file processing...");
+
+            foreach (string file in files)
+            {
+                FileInfo fileInfo = new FileInfo(file);
+                List<ForexEntry> entries = new List<ForexEntry>();
+                Console.WriteLine($"Processing file {file}...");
+                using (StreamReader reader = new StreamReader(file))
+                {
+                    string line;
+
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        string[] parts = line.Split(',');
+                        DateTime timestamp = DateTime.ParseExact(parts[0], "yyyyMMdd HHmmssfff", CultureInfo.InvariantCulture, DateTimeStyles.AssumeUniversal);
+                        entries.Add(new ForexEntry(timestamp, float.Parse(parts[1]), float.Parse(parts[2])));
+                    }
+                }
+
+                Console.WriteLine("Finished proceessing...");
+                Console.WriteLine("Outputting JSON file...");
+                string json = JsonConvert.SerializeObject(entries);
+                File.WriteAllText($"{fileInfo.Name.Replace(fileInfo.Extension, string.Empty)}.json", json);
+            }
+        }
+    }
+}
