fd00b595d4aae7ab7b8401392a9747cf7af13b18
Justin Mackenzie <jmackenzie1908@gmail.com>
2017-04-01 21:47:45 -0400

    Implemented import program for importing usd-cad daily averages.

---
+++ StockExchange.HistoricalForex.Import/StockExchange.HistoricalForex.ImportDailyAverage/ForexEntry.cs
@@ -0,0 +1,16 @@
+﻿namespace StockExchange.HistoricalForex.ImportDailyAverage
+{
+    public class ForexEntry
+    {
+        public float Low { get; set; }
+        public float High { get; set; }
+        public long Timestamp { get; set; }
+
+        public ForexEntry(long timestamp, float low, float high)
+        {
+            this.Timestamp = timestamp;
+            this.Low = low;
+            this.High = high;
+        }
+    }
+}
